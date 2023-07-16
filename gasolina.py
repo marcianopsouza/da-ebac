f3d49fe3c1be4c031a1932a435f7d674540d1e5d
marcianopsouza <marcianops2020@gmail.com>
2023-07-16 20:43:51 +0000

    arquivos alterados exercício 20

--- gasolina.py
+++
@@ -1,4 +0,0 @@
-grafico = sns.lineplot(data=tabela,x="dia",y="venda"
-grafico = sns.lineplot(data=tabela,x="dia",y="venda"
-grafico.set(title="Vendas de Gasolina por dia",xlabel="Dia",ylabel="Vendas"
-grafico.get_figure().savefig("gasolina.png")
